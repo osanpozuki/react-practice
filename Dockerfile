ce9244ecbe3b34c2914efd5f134fbb3fef42c825
tama <tama.osanpozuki@gmail.com>
2019-03-03 17:05:58 +0900

    styled-compnentsの追加

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ FROM node:10.15.0-alpine
 WORKDIR /app
 
 RUN apk update
-yarn install -global webpack
+
+RUN yarn install -global webpack
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { SubComponent } from "./sub-component";
+import styled, { css } from "styled-components";
+
+interface InterfaceMyProps {
+  primary?: boolean;
+  onClick: () => void;
+}
+
+const StyledButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello React!</h1>
+        <h1>Hello React</h1>
         <SubComponent name="My Counter for TypeScript" />
+        <StyledButton>aaa</StyledButton>
       </div>
     );
   }
